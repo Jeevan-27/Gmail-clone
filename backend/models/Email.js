26418d5318723303398773cf3b7148b22a9f6c36
Jeevan-27 <141016628+Jeevan-27@users.noreply.github.com>
2024-10-20 11:26:15 +0530

    Add files via upload

---
+++ backend/models/Email.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const EmailSchema = new mongoose.Schema({
+  to: { type: String, required: true },
+  from: { type: String, required: true },
+  subject: { type: String, required: true },
+  body: { type: String, required: true },
+  date: { type: Date, required: true },
+  starred: { type: Boolean, default: false },
+  bin: { type: Boolean, default: false },
+  type: { type: String, default: 'inbox' },
+});
+
+module.exports = mongoose.model('Email', EmailSchema,'inbox');
